2b9812fb757b245061a4f751337be0fef4f8bdc5
Luca Guerrero <Luka222@live.com.ar>
2023-06-23 22:56:26 -0300

    buttons
    add functional dark-mode and toggle nav btn

--- validacion.js
+++ validacion.js
@@ -1,1 +1,12 @@
 //Haz tú validación en javascript acá
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
+function changeColor() {
+  document.body.classList.toggle("dark-mode");
+}

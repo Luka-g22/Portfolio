0a81a1ca3cc472e12d6f1a6e0ddd7e6e57e2f9c5
Luca Guerrero <Luka222@live.com.ar>
2023-07-02 15:58:10 -0300

    softskills added

--- validacion.js
+++ validacion.js
@@ -1,18 +1,17 @@
-//Haz tú validación en javascript acá
+//NAV PAGE COLLAPSE
 function openNav() {
   document.getElementById("myNav").style.height = "100%";
 }
-
 function closeNav() {
   document.getElementById("myNav").style.height = "0%";
 }
 
+//TOGGLE MODE BUTTON
 function changeColor() {
   document.body.classList.toggle("dark-mode");
-}
+} //yoshi beach dark-mode
 
-
-
+//FIX NAV BUTTON
 var nav_btn = document.getElementById("fix_btn");
 function fixBoton() {
   if (window.scrollY > 100) {
@@ -24,3 +23,5 @@ function fixBoton() {
 window.onscroll = function () {
   fixBoton();
 };
+
+//FORMULARIO 
